9c6396b49ab89f39ce3737f0637214309243aadb
Ivana Krumlová <ivca.krumlova@gmail.com>
2022-05-10 13:51:48 +0200

    anketa: add reactions in correct order

--- anketa.py
+++ anketa.py
@@ -3,9 +3,10 @@ import emojis
 
 #
 async def anketa_cmd(ctx):
-    custom_emotes = re.findall(r'<a?:.+:\d{18}>', ctx.message.content)
+    #custom_emotes = re.findall(r'<a?:.+:\d{18}>', ctx.message.content)
     classic_emotes = list(emojis.get(ctx.message.content))
-    for emote in custom_emotes + classic_emotes:
+    emotes = re.findall(r'<a?:.+:\d{18}>|' + "|".join(classic_emotes), ctx.message.content)
+    for emote in emotes:
         await ctx.message.add_reaction(emoji=emote)
 
 # Simple yes/no vote
