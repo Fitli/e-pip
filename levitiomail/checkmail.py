f8f76eb73bb738d4465499b66c8bd160e99d3b46
Petr Zelina <zelina.pe@gmail.com>
2024-08-11 21:23:10 +0200

    add signup button for trips

--- levitiomail/checkmail.py
+++ levitiomail/checkmail.py
@@ -11,10 +11,16 @@ import discord
 import base64
 import subprocess
 import regex
+
+from sqlitedict import SqliteDict
+
 # from markdownify import markdownify as md
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://mail.google.com/"]
+
+USER_LINK_DB = "levitio_links.db"
+PERSONAL_TIMEOUT = 30
 
 
 class SignUpButton(discord.ui.View):
@@ -24,22 +30,92 @@ class SignUpButton(discord.ui.View):
     @discord.ui.button(label="Přihlásit se v Levitiu", custom_id="SignUpButton_0000",
                        row=0, style=discord.ButtonStyle.primary)
     async def signup_callback(self, interaction, button):
-        # TODO check if user link is stored, otherwise display modal asking for a link to Levitio trip overview
-        await interaction.response.send_message(
-            "[Klikni sem pro tvůj Levitio přehled](https://public.levitio.com/units/dcbf1eb0-cd5f-49dd-bb6c-2e8f8743a35f?email=foton.web@stezka.org&verification=6f667d6667277e6c6b497a7d6c73626827667b6e) .", ephemeral=True, delete_after=30, suppress_embeds=True,
+
+        uid = interaction.user.id
+        # print(uid)
+        with SqliteDict(USER_LINK_DB) as db:
+            # print(len(db))
+            link = db.get(uid)
+
+        if link is not None:
+            await show_signup_link(interaction, link)
+        else:
+            await interaction.response.send_modal(LevitioSignupModal(user_id="", title="Přihlásit se v Levitiu"))
+
+async def show_signup_link(interaction, link):
+    await interaction.response.send_message(
+        "Tady se můžeš přihlásit nebo změnit nastavení.",
+        # "[Klikni sem pro tvůj Levitio přehled](https://public.levitio.com/units/dcbf1eb0-cd5f-49dd-bb6c-2e8f8743a35f?email=foton.web@stezka.org&verification=6f667d6667277e6c6b497a7d6c73626827667b6e) .",
+        view=PersonalSignupView(link),
+        ephemeral=True, delete_after=PERSONAL_TIMEOUT, suppress_embeds=True,
+    )
+
+class PersonalSignupView(discord.ui.View):
+    def __init__(self, link):
+        super().__init__(timeout=PERSONAL_TIMEOUT)
+
+        b1 = discord.ui.Button(
+            label='Tvůj Levitio přehled',
+            url=link
         )
-        # await interaction.response.send_modal(LevitioSignupModal(user_id="", title="Přihlásit se v Levitiu"))
+        self.add_item(b1)
+
+        b2 = SettingsButton(
+            label="⚙",
+            style=discord.ButtonStyle.red,
+        )
+        self.add_item(b2)
+
+class SettingsButton(discord.ui.Button):
+    async def callback(self, interaction):
+        await interaction.response.send_modal(LevitioSignupModal(user_id="", title="Přihlásit se v Levitiu"))
 
 class LevitioSignupModal(discord.ui.Modal):
     def __init__(self, user_id, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        self.add_item(discord.ui.TextInput(label="Short Input https://www.seznam.cz Pes"))
-        # self.add_item(discord.ui.Button(label="Levitio", url="https://www.seznam.cz"))
+        desctext = "Aby toto tlačítko fungovalo, potřebujeme odkaz na tvůj Levitio přehled. (Klikni na libovolnou pozvánku z e-mailu -> pravým tlačítkem na Přehled akcí (vlevo) -> kopírovat adresu odkazu) Jakmile ho jednou zadáš do následujícího políčka, E-Pip si ho zapamatuje a bude ti stačit jenom kliknout sem; nebudeš muset chodit na e-mail."
+
+        self.add_item(discord.ui.TextInput(
+            label="Vysvětlení",
+            default=desctext,
+            required=False,
+            style=discord.TextStyle.long,
+        ))
+
+        self.new_link_input = discord.ui.TextInput(
+            label="Odkaz na tvůj Levitio přehled",
+            # style=discord.TextStyle.,
+            required=True,
+        )
+        self.add_item(self.new_link_input)
 
 
-    async def callback(self, interaction: discord.Interaction):
-        pass
+    async def on_submit(self, interaction: discord.Interaction):
+        uid = interaction.user.id
+        link = self.new_link_input.value
+        # print(uid)
+        # print(self.new_link_input.value)
+
+        warning = ""
+        if not link.startswith("https://public.levitio.com/units/"):
+            warning="\n\nZdá se mi, že by ten odkaz měl vypadat jinak. Zkopíroval jsi správný odkaz?\nKdyžtak si ho můžeš změnit opětovným kliknutím na \"Přihlásit se v Levitiu\" -> Nastavení (⚙)."
+
+        if link.startswith("http"):
+            print(f"Remembering for {uid}: {self.new_link_input.value}")
+            with SqliteDict(USER_LINK_DB) as db:
+                db[uid] = self.new_link_input.value
+                db.commit()
+
+            await interaction.response.send_message(
+                f'Zapamatoval jsem si tvůj [odkaz]({self.new_link_input.value}).' + warning,
+                ephemeral=True, delete_after=PERSONAL_TIMEOUT, suppress_embeds=True,
+            )
+        else:
+            await interaction.response.send_message(
+                warning,
+                ephemeral=True, delete_after=PERSONAL_TIMEOUT, suppress_embeds=True,
+            )
 
 
 async def post_email(data, channel):
@@ -52,7 +128,7 @@ async def post_email(data, channel):
     )
     await channel.send(
         embed=embed,
-        # view=SignUpButton() # TODO personalised levitio link
+        view=SignUpButton()
     )
 
 def convert_mail(html):
@@ -79,7 +155,7 @@ def convert_mail(html):
 
     # converted = regex.sub(r"(?=[\S])\*\*", " **", converted)
 
-    converted += "\n\n## *Nezapomeňte se přihlásit na vašem e-mailu!*"
+    # converted += "\n\n## *Nezapomeňte se přihlásit na vašem e-mailu!*"
     return converted
 
 async def check_emails(channel):
@@ -133,7 +209,6 @@ async def check_emails(channel):
             userId='me', id=message["id"], body={'removeLabelIds': ['UNREAD']}).execute()
 
   except HttpError as error:
-    # TODO(developer) - Handle errors from gmail API.
     print(f"An error occurred: {error}")
 
 
